af3be67aed9501caaaa8253db691576941086ec7
User_4 <elshodxushvaqtov07@gmail.com>
2023-10-24 19:03:40 +0500

    signup layout

--- app/src/main/java/uz/itschool/handybook/fragments/LoginFragment.kt
+++ app/src/main/java/uz/itschool/handybook/fragments/LoginFragment.kt
@@ -1,6 +1,7 @@
 package uz.itschool.handybook.fragments
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View

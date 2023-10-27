bb7375b84fd7b2b19f5b2a613ca738d8884923fc
behruzbek2Gulomov <120156394+behruzbek2Gulomov@users.noreply.github.com>
2023-10-27 09:51:43 +0500

    Behruz

--- app/src/main/java/uz/itschool/handybook/fragments/BookInfoFragment.kt
+++ app/src/main/java/uz/itschool/handybook/fragments/BookInfoFragment.kt
@@ -29,20 +29,11 @@ class BookInfoFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         _binding= FragmentBookInfoBinding.inflate(inflater,container,false)
-
+        
         return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookInfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: Book) =
             BookInfoFragment().apply {
